832982a21f1534c1e38f837fd512fd4aed94c195
William Eppel <william.eppel@student.htw-berlin.de>
2022-07-06 17:58:27 +0200

    mainActivity

--- app/src/main/java/de/htw_berlin/quiz/MainActivity.kt
+++ app/src/main/java/de/htw_berlin/quiz/MainActivity.kt
@@ -1,11 +1,39 @@
 package de.htw_berlin.quiz
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+/*
+    override fun onBackPressed() {
+
+
+        //HomeScreen
+        /*
+        val startMain = Intent(Intent.ACTION_MAIN)
+        startMain.addCategory(Intent.CATEGORY_HOME)
+        startMain.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(startMain)
+         */
+
+
+        //back to MainActiviy ( TitleFragment )
+        /*
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+         */
+
+    }
+*/
+
 }
